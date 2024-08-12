0696e0830f50f6a4d4b21604ab9874cdd7345403
Ramesh <ramesh.kitkat94@gmail.com>
2024-08-12 15:32:06 +0530

    first commit

---
+++ app/src/main/java/com/example/cupcake/ui/SummaryScreen.kt
@@ -0,0 +1,116 @@
+package com.example.cupcake.ui
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.OutlinedButton
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.cupcake.R
+import com.example.cupcake.data.OrderUiState
+import com.example.cupcake.ui.components.FormattedPriceLabel
+import com.example.cupcake.ui.theme.CupcakeTheme
+
+@Composable
+fun OrderSummaryScreen(
+    modifier: Modifier = Modifier,
+    onCancelButtonClicked: () -> Unit,
+    onSendButtonClicked: (String, String) -> Unit,
+    orderUiState: OrderUiState,
+) {
+    val resources = LocalContext.current.resources
+
+    val numberOfCupcakes = resources.getQuantityString(
+        R.plurals.cupcakes,
+        orderUiState.quantity,
+        orderUiState.quantity
+    )
+    //Load and format a string resource with the parameters.
+    val orderSummary = stringResource(
+        R.string.order_details,
+        numberOfCupcakes,
+        orderUiState.flavor,
+        orderUiState.date,
+        orderUiState.quantity
+    )
+    val newOrder = stringResource(R.string.new_cupcake_order)
+    //Create a list of order summary to display
+    val items = listOf(
+        // Summary line 1: display selected quantity
+        Pair(stringResource(R.string.quantity), numberOfCupcakes),
+        // Summary line 2: display selected flavor
+        Pair(stringResource(R.string.flavor), orderUiState.flavor),
+        // Summary line 3: display selected pickup date
+        Pair(stringResource(R.string.pickup_date), orderUiState.date)
+    )
+
+    Column(
+        modifier = modifier,
+        verticalArrangement = Arrangement.SpaceBetween
+    ) {
+
+        Column(
+            modifier = Modifier.padding(dimensionResource(id = R.dimen.padding_medium)),
+            verticalArrangement = Arrangement.spacedBy(dimensionResource(id = R.dimen.padding_small))
+        ) {
+            items.forEach { item ->
+                Text(text = item.first)
+                Text(text = item.second, fontWeight = FontWeight.Bold)
+                HorizontalDivider(thickness = dimensionResource(id = R.dimen.thickness_divider))
+            }
+            Spacer(modifier = Modifier.height(dimensionResource(id = R.dimen.padding_small)))
+            FormattedPriceLabel(
+                subtotal = orderUiState.price,
+                modifier = Modifier.align(Alignment.End)
+            )
+        }
+
+        Row(modifier = Modifier.padding(dimensionResource(id = R.dimen.padding_medium))) {
+
+            Column(verticalArrangement = Arrangement.spacedBy(dimensionResource(id = R.dimen.padding_small))) {
+                Button(
+                    onClick = { onSendButtonClicked(newOrder, orderSummary) },
+                    modifier = Modifier.fillMaxWidth()
+                ) {
+                    Text(text = stringResource(id = R.string.send))
+                }
+
+                OutlinedButton(
+                    onClick = onCancelButtonClicked,
+                    modifier = Modifier.fillMaxWidth()
+                ) {
+                    Text(text = stringResource(id = R.string.cancel))
+                }
+            }
+
+        }
+    }
+
+}
+
+@Preview(showBackground = true)
+@Composable
+fun OrderSummaryPreview() {
+    CupcakeTheme {
+        OrderSummaryScreen(
+            orderUiState = OrderUiState(0, "Test", "Test", "$300.00"),
+            onSendButtonClicked = {subject:String, summery:String -> },
+            onCancelButtonClicked = {},
+            modifier = Modifier.fillMaxHeight()
+        )
+    }
+}
